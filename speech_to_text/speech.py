4f0ccf83bf20d436d8dfd3b99e2cb736ff161ded
reuelrds <reuelrds@gmail.com>
2020-02-01 20:06:15 +0530

    changed name to avoid import conflict with the SpeechRecognition Library

---
+++ speech_to_text/speech.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 21:28:01 2019
+
+@author: reuelrds
+"""
+
+import speech_recognition as sr
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("\nSay something!")
+        audio = r.listen(source)
+        print("Transcribing...")
+        
+    try:
+        transcript = r.recognize_google(audio)
+        print("Transcript: ",transcript)
+        return transcript
+        
+
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+        return "Could not understand audio"
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        return "Could not get results"
+
+if __name__=="__main__":
+    print(listen())
