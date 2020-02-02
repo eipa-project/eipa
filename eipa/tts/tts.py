d61afcdd83f1b2f143bef22bee09d0f166ed19d6
reuelrds <reuelrds@gmail.com>
2020-02-02 12:56:34 +0530

    Added Text to Speech

---
+++ eipa/tts/tts.py
@@ -0,0 +1,32 @@
+"""
+Created on Sat Nov  2 00:39:03 2019
+
+@author: reuelrds
+"""
+
+from pathlib import Path
+from time import sleep
+
+from gtts import gTTS
+import pygame
+
+
+def play(text):
+
+    pygame.mixer.init()
+
+    path = str(Path(__file__).parent / "outputs" / "output.mp3")
+    print(path)
+
+    output = gTTS(text, lang="en-us", slow=False)
+    output.save(path)
+
+    pygame.mixer.music.load(path)
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        sleep(1)
+
+
+if __name__ == "__main__":
+    play("The quick brown fox jumped over the lazy dog.")
